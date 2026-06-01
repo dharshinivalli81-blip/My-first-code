327a6ae0b61752b1e43421ba936a811b54fdb24b
dharshinivalli81-blip <dharshinivalli81@gmail.com>
2026-06-02 01:14:30 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,13 @@
+name = "Dharshini"
+college = "SASTRA University"
+course = "BSc Computer Science"
+batch = "2026-2029"
+dream_company = "Zoho"
+
+print(f"Name: {name}")
+print(f"College: {college}")
+print(f"Course: {course}")
+print(f"Batch: {batch}")
+print(f"Dream Job: Software Engineer at {dream_company}")
+print("Day 1 Completed ✅ First GitHub commit done!")
+print("Future Zoho Engineer in making 🚀")
